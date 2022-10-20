120519bf42792c7575a1487fd8ebefa2859888a9
Vinh Pham <33125440+Jorkendy@users.noreply.github.com>
2022-10-20 14:19:33 +0700

    Test touchMoveStopPropagation

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         preventClicks={true}
         preventClicksPropagation={true}
         modules={[Mousewheel]}
-        freeMode
+        touchMoveStopPropagation
       >
         <SwiperSlide>
           <Banner />
